f8f4fc0aef6a3b8453643216790c516e182787fa
lchaconc <luis.chacon.campos@gmail.com>
2023-03-02 10:25:15 -0600

    corrección en webpack de producción

--- plantillas/dnd_imagen_area/webpack.config.app.prod.js
+++ plantillas/dnd_imagen_area/webpack.config.app.prod.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require("copy-webpack-plugin");
 const ZipPlugin = require("zip-webpack-plugin");
 const path = require("path");
-const crypto = require ("crypto");
+
 
 
 module.exports = (idApp)=> {
@@ -11,17 +11,16 @@ module.exports = (idApp)=> {
 
 
   return {
-    path: path.resolve( __dirname,  `../../dist/${idApp}`),
-    output: {        
-        //path: path.resolve( __dirname,  `dist/${crypto.randomBytes(6).toString('hex')}`         ),
-        path: path.resolve( __dirname,  `dist/${idApp}`         ),
+    entry: "./plantillas/dnd_imagen_area/src/main.js",    
+    output: {                
+        path: path.resolve( __dirname,  `../../dist/${idApp}`),
         filename: "bundle.[contenthash].js"
     },
     mode: "production",
     resolve: {
       extensions: [".js", ".json"]
     },
-    plugins: [new HtmlWebpackPlugin({template: "./plantillas/dnd_imagen_area/public/index.html"}),
+    plugins: [new HtmlWebpackPlugin({template: "./plantillas/dnd_imagen_area/public/index.html"}),    
     new CopyPlugin({
         patterns: [
           { from: "./plantillas/dnd_imagen_area/public/assets", to: "./assets" },
