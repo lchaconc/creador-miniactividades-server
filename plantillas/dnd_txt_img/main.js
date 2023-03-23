845a28e4bb650e4c0962ddfdf645564b0af0481c
lchaconc <luis.chacon.campos@gmail.com>
2023-03-23 14:20:27 -0600

    Integración de actividad dnd texto imagen Fase 1: -Creación de carpeta y archivos del proyeto -Creación del esuqema (Estructura de datos) -Definición del modelo

---
+++ plantillas/dnd_txt_img/main.js
@@ -0,0 +1,28 @@
+import * as utils from "./utils.js";
+import eventHandlers from "./handlers.js";
+import data from "./data/data.json";
+import "./css/master.css";
+import "animate.css";
+
+
+const textos = data.textos;
+const areasImagen = data.areasImagen;
+const cajasTexto = data.cajasTexto;
+
+
+onload =()=> setup();
+
+async function setup() {
+  console.log("setup");
+  
+  console.log(textos, areasImagen, cajasTexto);
+  
+  utils.eliminarElemento("divCargandoDatos");
+  utils.renderTexto("divTitulo1", textos.titulo, "h1" );
+  utils.renderTexto("divInstrucciones", textos.instruccion, "p", "alert alert-secondary" );
+  
+  
+  utils.renderAreasImg(areasImagen);
+  utils.renderCajasTexto(cajasTexto);
+  eventHandlers(cajasTexto, textos );  
+}
---
+++ src/models/model.dnd-txt-img.mjs
@@ -0,0 +1,70 @@
+import { Schema, model } from "mongoose";
+
+const dndTextoImagenSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+    match: /^[a-zA-Z0-9.@]+$/,
+    maxlength: 128,
+  },
+  tipo: {
+    type: String,
+    default: "dnd_txt_img",
+  },
+  fechaCreacion: {
+    type: Date,
+    default: Date.now,
+  },
+  textos: {
+    titulo: {
+      type: String,
+      trim: true,
+      maxlength: 128,
+    },
+    instrucciones: {
+      type: String,
+      trim: true,
+      maxlength: 256,
+    },
+    retroCorrecta: {
+      type: String,
+      trim: true,
+      maxlength: 64,
+    },
+    retroIncorrecta: {
+      type: String,
+      trim: true,
+      maxlength: 64,
+    }   
+  },
+  cajasTexto: [
+    {  
+      texto: {
+        type: String,
+        trim: true,
+        maxlength: 512,
+      },
+      idArea: {
+        type: String,
+        trim: true,
+        maxlength: 32,
+      }
+  
+    }
+  ],
+  areasImagen: [
+    {
+      alt: {
+        type: String,
+        trim: true,
+        maxlength: 32,
+      },
+      archivo: {
+        type: String,
+        trim: true,
+        maxlength: 32,
+      }
+    }
+  ]
+});
